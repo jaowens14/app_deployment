79cdbb4a37bfefe3b95c655da79f6f51b2bec0ee
jaowens14 <jaowens14@protonmail.com>
2023-05-09 16:01:19 -0600

    upload

--- build_start_app.sh
+++ build_start_app.sh
@@ -2,4 +2,4 @@ docker build --tag test_app test_app/
 docker build --tag nginx nginx/
 
 docker run -d -p 8000:8502 test_app
-docker run -d -p 9000:8502 nginx
+docker run -d -p 80:80 nginx
--- stop_app.sh
+++ stop_app.sh
@@ -1,2 +1,1 @@
 docker stop $(docker ps -a -q)
-docker system prune -a
