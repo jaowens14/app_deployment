c4b748c7441a0830f4cf1569e465b84edc59abac
jaowens14 <jaowens14@protonmail.com>
2023-05-10 12:00:19 -0600

    moved everything to port 80

--- build_start_app.sh
+++ build_start_app.sh
@@ -4,4 +4,4 @@ docker build --tag nginx nginx/
 docker network create app-net
 
 docker run --net app-net --name test_app -d test_app
-docker run --net app-net --name nginx -d -p 8000:80 nginx
+docker run --net app-net --name nginx -d -p 80:80 nginx
