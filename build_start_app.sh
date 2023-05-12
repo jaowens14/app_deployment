c1babb0e4f21a4b372a5d9e638565a2b3a77f0c5
jaowens14 <jaowens14@protonmail.com>
2023-05-12 07:33:56 -0600

    new stuff

--- build_start_app.sh
+++ build_start_app.sh
@@ -3,5 +3,5 @@ docker build --tag nginx nginx/
 
 docker network create app-net
 
-docker run --net app-net --name test_app -d test_app
-docker run --net app-net --name nginx -d -p 80:80 nginx
+docker run --net app-net --name test_app --hostname test_app -d -p 8502:8502 test_app
+docker run --net app-net --name nginx --hostname nginx -d -p 8000:8000 nginx
--- test_app/app.py
+++ test_app/app.py
@@ -1,7 +1,7 @@
 import flet
 import os
 from flet import IconButton, Page, Row, TextField, icons
-DEFAULT_FLET_PORT = 80
+DEFAULT_FLET_PORT = 8502
 
 def main(page: Page):
     page.title = "Flet counter example"
@@ -31,4 +31,4 @@ def main(page: Page):
 
 flet_port = int(os.getenv("FLET_PORT", DEFAULT_FLET_PORT))
 
-flet.app(target=main, view=flet.WEB_BROWSER, port=flet_port)
+flet.app(target=main, view=flet.WEB_BROWSER, port=8502)
