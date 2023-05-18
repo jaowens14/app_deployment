ba62d787cd095cf931c1e6a68415493b8b232503
jaowens14 <jaowens14@protonmail.com>
2023-05-18 09:43:30 -0600

    removed scripts

--- build_start_app.sh
+++
@@ -1,7 +0,0 @@
-docker build --tag test_app test_app/
-docker build --tag nginx nginx/
-
-docker network create app-net
-
-docker run --net app-net --name test_app --hostname test_app -d -p 8502:8502 test_app
-docker run --net app-net --name nginx --hostname nginx -d -p 8000:8000 nginx
