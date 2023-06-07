2d93ce6f72c2de347f5242a6e1d06733193a5918
Jacob Owens <jaowens14@protonmail.com>
2023-06-07 07:03:50 -0600

    working on connecting python to postgres

--- app/database.py
+++ app/database.py
@@ -3,4 +3,31 @@ import psycopg2
 # need to write stuff to save dog bone data to this database
 
 
-connection = psycopg2.connect(dsn)
+import psycopg2
+from psycopg2 import Error
+
+try:
+    connection = psycopg2.connect(user="postgres",
+                                  password="test",
+                                  host="localhost",
+                                  port="5432",
+                                  database="example")
+
+    cursor = connection.cursor()
+    # SQL query to create a new table
+    create_table_query = '''CREATE TABLE mobile
+          (ID INT PRIMARY KEY     NOT NULL,
+          MODEL           TEXT    NOT NULL,
+          PRICE         REAL); '''
+    # Execute a command: this creates a new table
+    cursor.execute(create_table_query)
+    connection.commit()
+    print("Table created successfully in PostgreSQL ")
+
+except (Exception, Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
